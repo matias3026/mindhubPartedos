45e58360b631c3bf937d41a541d3eae7bec1e16e
matias <matias3026@gmail.com>
2023-02-27 18:22:21 -0300

    confrmar botones

--- 1.Matias_Damiani_T2_M2 js/js/main.js
+++ 1.Matias_Damiani_T2_M2 js/js/main.js
@@ -1,30 +1,25 @@
-
 import data from './data.js';
 
 
 let contenedorDeTarjetas = document.getElementById("recenPorMi");
 
 for(let event of data.events){
-    let cardvar = document.createElement("div");
-    cardvar.className = "col";
-    cardvar.innerHTML = `
+    let card = document.createElement("div");
+    card.className = "row";
+    card.innerHTML = `
 
-        <div class="card h-100">
-            <img src="${event.image}" class="card-img-top" alt="..."></img>
-            <div class="card-body text-center">
-                <h3 class="card-title">${event.name}</h3>
-                <p class="card-text">${event.description}</p>
-                <div class="card_bottom">
-                    <h5>Price:  ${event.price}</h5>
-                    <button class="card_bottom_button">
-                        Ver
-                    </button>
-                </div>
-            </div>
-        </div>
+    <img src="${event.image}" class="card-img-top" alt="maraton">
+    <div class="card-body" >
+    <h5 class="card-title">${event.name}</h5>
+    <p class="card-text">${event.description} </p>
+    <div class="precio">
+        <p class="Price">${event.price}</p>
+        <a href="#" class="btn btn-primary">xxxxxxxxxxx</a>
+    </div>
+    </div>
     `;
 
-    contenedorDeTarjetas.appendChild(cardvar); //Luego de generar toda la variable card, con appendChild se la sumo al div de id #cards-row que 
-    //tengo como contenedor principal de las cards
-}
+   
 
+    contenedorDeTarjetas.appendChild(card)
+}
