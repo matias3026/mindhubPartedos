1978cbfb23a775afb725878476049fecd30686c0
matias3026 <88506234+matias3026@users.noreply.github.com>
2023-03-23 16:57:56 -0300

    se cambia a un mejor codigo con ayuda del compa maxi

--- 1.Matias_Damiani_T4_M2 js/js/up.js
+++ 1.Matias_Damiani_T4_M2 js/js/up.js
@@ -2,24 +2,27 @@
 /*1° - Guardamos la direccion de la API en una variable*/
 let urlAPI = 'https://mindhub-xj03.onrender.com/api/amazing';
 
-/*6° - Creo una variable tipo array vacio para guardar los eventos*/
+/*6° - Creo una variable tipo array vacio para guardar los datos de los eventos*/
 let eventos = [];
 
-/*11° - Creo una variable tipo array vacio para guardar las categorias de los 
+/*7 - Creo un variable para almacenar la fecha con la cual vamos a separar los 
 eventos*/
+let currentData;
+
+/*13° - Creo una variable tipo array vacio para guardar los tipos de eventos*/
 let categorias = [];
 
-/*16° - Creamos una variable que toma los datos del input de busqueda y una 
+/*18° - Creamos una variable que toma los datos del input de busqueda y una 
 varible que toma el estado del boton*/
 /*Escucho el input*/
 let inputBusqueda = document.querySelector("input[name=search]");
 /*Escucho el boton*/
 let boton = document.querySelector("button[name=button]");
 
-/*18° - Creo una variable que va a contener los checkboxes tildados*/
+/*20° - Creo una variable que va a contener los checkboxes tildados*/
 let checkboxes;
 
-/*22° - Creo una variable que informara un mensaje cuando no se encuentren los 
+/*24° - Creo una variable que informara un mensaje cuando no se encuentren los 
 eventos buscados*/
 let mensaje = document.querySelector(".mensaje")
 
@@ -34,69 +37,73 @@ async function getEventos(){
         archivo json del servidor*/
         let dataAPI = await response.json();
         
-        /*Visualizo los datos obtenidos por consola*/
+        /*Visualizo los datos obtenidos por consola para verificar el formato de 
+        los mismos*/
         //console.log(dataAPI);
         
-        /*7° - Completo el array de eventos con los datos obtenidos del API*/
-        for (let evento of dataAPI.events){
-            eventos.push(evento);
-        }
+        /*8° - Asigno a la variable currentData la fecha indicada en DataApi*/
+        currentData = new Date(dataAPI.currentDate);
         
+        /*9° - Completo el array de eventos posteriores a la fecha contenida en 
+        currentDate con los datos obtenidos del API*/
+        for (let evento of dataAPI.events){
+            let eventDate = new Date(evento.date);
+            if(eventDate>currentData){
+                eventos.push(evento);
+            }   
+        }
+
         /*Visualizo el array con los eventos por consola*/
         //console.log(eventos);
-        
-        /*8° Ahora que tengo los eventos en un array puedo llamar a una funcion 
-        que renderize las cards con los datos de los eventos*/
+
+        /*10° Ahora que tengo los eventos en un array puedo hacer una funcion que 
+        renderize las cards con los datos de los eventos*/
         renderCards(eventos);
 
-        /*12° Extraemos del array de eventos los tipos de eventos, llamando a
+        /*14° Extraemos del array de eventos las categorias de los eventos, usando 
         una funcion*/
         categorias = extractCategory(eventos);
 
-        /*14° Llamo a una funcion que me cree el HTML de los checkboxes*/
+        /*16° Llamo a una funcion que me cree el HTML de los checkboxes*/
         renderCheckboxes(categorias);
 
-        /*19° - Agrego un eventListener a los checkboxes creados de forma dinamica y 
+        /*21° - Agrego un eventListener a los checkboxes creados de forma dinamica y 
         comienzo a leer el estado de cada uno de los mismos*/
         checkboxes = document.querySelectorAll("input[type=checkbox]");
         checkboxes.forEach(checkbox => {
         checkbox.addEventListener("change",()=>{
-            /*Visualizo los datos obtenidos por consola*/
+            /*Visualizo el array con los eventos por consola*/
             //console.log(checkbox.value);
             renderSearch();
         });
-    });
+    });    
 
     } catch (error) {
         console.log(error.message);
     }
-
 }
+
 
 /*5° - Llamo a la funcion para corroborar la conexion, los datos obtenidos y 
 la funcionalidad de la logica*/
 getEventos();
 
+
 /*Funciones*/
-/*9° - Creo la funcion que renderiza e injecta las cards en el documento HTML*/
+/*11° - Creo la funcion que renderiza e injecta las cards en el documento HTML*/
 function renderCards(eventos){
-    /*Defino el contenedor*/
     let container = document.querySelector(".row");
-    /*Creo una variable para almacenar el html*/
     let htmlCards = "";
-    /*Recorro el evento y armo la estructura HTML llamando a una funcion*/
     eventos.forEach(evento => htmlCards += createdCard(evento));
-    /*Inserto el HTML a la pagina*/
     container.innerHTML = htmlCards
 }
 
-/*10° - Creo la funcion que crea la card*/
+/*12° - Creo la funcion que crea la card*/
 function createdCard(evento){
-    /*Retorno la card armada*/
     return `<div class="col-sm-12 col-md-6 col-xl-3 py-2">
     <div class="card">
         <div class="card-body text-center">
-            <img src="${evento.image}" class="card_img card-img-top" alt="foto ${evento.name}">
+            <img src="${evento.image}" class="card_img card-img-top" alt="${evento.name}">
             <h5 class="card-title mt-2 fw-bold ">${evento.name}</h5>
             <p class="card-text">${evento.description}</p>
             <div class="d-flex justify-content-between mt-5">
@@ -107,9 +114,9 @@ function createdCard(evento){
         </div>
     </div>
 </div>`;
-}
+};
 
-/*13° - Creo una funcion que extrae las categorias de los eventos del array 
+/*15° - Creo una funcion que extrae las categorias de los eventos del array 
 obtenido de la API y los guarda en un array*/
 function extractCategory(eventos){
     let category = [];
@@ -118,12 +125,12 @@ function extractCategory(eventos){
             category.push(evento.category);
         }
     });
-    /*Visualizo los datos obtenidos por consola*/
+    /*Visualizo el array con los eventos por consola*/
     //console.log(category);
     return category;
 }
 
-/*15° - Creo la funcion que renderiza, arma e injecta los checkboxes en el html*/
+/*17° - Funcion que renderiza, arma e injecta los checkboxes en el html*/
 function renderCheckboxes(categorias){
     let container = document.querySelector(".main_form_izq");
     container.innerHTML = categorias.map(categoria =>
@@ -134,15 +141,15 @@ function renderCheckboxes(categorias){
         </label>`).join("");
 }
 
-/*17° - Agrego un eventListener a boton para renderize nuevamente las card cada vez que 
+/*19° - Agrego un eventListener a boton para renderize nuevamente las card cada vez que 
 se preciona buscar*/
 boton.addEventListener("click",()=>{
-    /*Visualizo los datos obtenidos por consola*/
+    /*Visualizo el array con los eventos por consola*/
     //console.log(inputBusqueda.value);
     renderSearch();
 });
 
-/*20° - Creo una funcion que crea un array y devuelve el nombre de los checkboxes 
+/*22° - Funcion que crea un array y devuelve el nombre de los checkboxes 
 checkeados*/
 function getChequeados(){
     //Creo un array que contendra los checkboxes con estado checked
@@ -155,20 +162,22 @@ function getChequeados(){
     return chequeados;
 };
 
-/*21° - Creo una funcion que realiza una busqueda segun el filtro que se encuentra 
+/*23° - Funcion que realiza una busqueda segun el filtro que se encuentra 
 activo*/
 function renderSearch(){
-    /*Creamos las variables que contendran los valores tomados desde la web*/
+    /*Creamos las funciones que contendran los valores tomados desde la web*/
     let textoBusqueda = inputBusqueda.value;
     let categoriasChequeados = getChequeados();
     mensaje.innerHTML="";
-    /*Visualizo los datos obtenidos por consola*/
+
+    /*Visualizo el array con los eventos por consola*/
     //console.log(textoBusqueda);
     //console.log(categoriasChequeados);
+
     /*Creo una variable para guardar los resultados obtenidos segun las
     selecciones realizadas*/
     let resultados = eventos.filter(evento => (evento.name.toLowerCase().includes(textoBusqueda.toLowerCase()))||(evento.description.toLowerCase().includes(textoBusqueda.toLowerCase())));
-    /*Visualizo los datos obtenidos por consola*/
+    /*Visualizo el array con los eventos por consola*/
     //console.log(resultados);
     if(categoriasChequeados.length>0){
         resultados = resultados.filter(evento =>{
@@ -178,10 +187,12 @@ function renderSearch(){
             }
             return pasaFiltro;
         });
+        
     }
     renderCards(resultados)
     if(resultados.length==0){
         mensaje.innerHTML= `<h5>No se encontraron eventos disponibles</h5>`
     }
 };
-    
+
+
